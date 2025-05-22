f2d437a02eef147fc4e7f2d5c097c64ee488e9bc
noob-ubaid <ubaidurrahman661@gmail.com>
2025-05-22 20:28:43 +0200

    added delete functionality

--- src/Pages/MyRecipes/MyRecipe.jsx
+++ src/Pages/MyRecipes/MyRecipe.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 import { Link } from "react-router";
+import Swal from "sweetalert2";
 
 const Mycard = ({ card }) => {
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`https://recipe-server-liard.vercel.app/addrecipes/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your recipe has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+      }
+    });
+  };
   return (
     <div className="border border-[#0F0F0F26] p-4 md:p-6 rounded-md">
       <div>
@@ -13,6 +41,12 @@ const Mycard = ({ card }) => {
       </div>
       <h3 className="mt-4 text-xl font-semibold">{card.name}</h3>
       <p className="text-[#0F0F0F99] font-medium mt-2">
+        Ingredients : {card.ingredients}{" "}
+      </p>
+      <p className="text-[#0F0F0F99] font-medium mt-2">
+        Instructions : {card.instructions}{" "}
+      </p>
+      <p className="text-[#0F0F0F99] font-medium mt-2">
         Preparation Time : {card.time} Minutes
       </p>
       <p className="text-[#0F0F0F99] font-medium mt-2">
@@ -21,8 +55,17 @@ const Mycard = ({ card }) => {
       <p className="text-[#0F0F0F99] font-medium mt-2">
         Category : {card.category}{" "}
       </p>
-      <div className="mt-4 w-full ">
-        
+
+      <div className="mt-4 w-full flex items-center gap-8 justify-center ">
+        <button className="rounded-md md:px-6 w-full md:py-3 px-4 py-2 text-[#09982F] bg-[#09982F1A] ">
+          Edit
+        </button>
+        <button
+          onClick={() => handleDelete(card._id)}
+          className="rounded-md md:px-6 w-full md:py-3 px-4 py-2 text-[#FF0000] bg-[#FF000010] "
+        >
+          Delete
+        </button>
       </div>
     </div>
   );
